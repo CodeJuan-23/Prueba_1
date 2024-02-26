5fb1d768067b3644ed68ea06f626b0757eef3620
CodeJuan-23 <i2226338@continental.edu.pe>
2024-02-26 12:24:53 -0500

    array

---
+++ Array1.php
@@ -0,0 +1,12 @@
+<?php
+   $coches = array("VOLVO", "TOYOTA", "RENOLT", "SUSUKI");
+   for($i=0; $i<4; $i++){
+       echo $coches[$i]."\n";  
+   }
+   $coches[1]="FORD";
+   for($i=0; $i<4; $i++){
+       echo $coches[$i]."\n";  
+   }
+   
+ ?>
+ 
